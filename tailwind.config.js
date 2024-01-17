a0e07ea4509248e4f3f076ccd28ca5a4173272a6
houwenchang-c <houwenchang-c@360.cn>
2024-01-17 22:31:57 +0800

    offical website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+module.exports = {
+  content: ['./src/**/*.{astro,js,jsx,ts,tsx}'],
+  // 其他Tailwind配置...
+};
